1c1babf65b8dadc379c317230cf20af0469989d4
Gabriel Jimenez <cuztomiz@gmail.com>
2020-11-10 03:55:24 -0500

    adding auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
+// styles
+
+// import "./assets/css/paper-kit.css";
+// import "./assets/scss/paper-kit.scss?v=1.2.0";
+// import NucleoIcons from "./views/NucleoIcons.js";
+
+
 
 ReactDOM.render(
   <React.StrictMode>
